152ef9a972886fbca6ec0ff2d8dafaf6725530fa
данечка ыыыы <akkhazriel@gmail.com>
2023-08-01 22:05:47 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// open burger menu
+const burger = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+const body = document.body;
+
+if (burger && menu) {
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('_active');
+        menu.classList.toggle('_active');
+        body.classList.toggle('_lock');
+    })
+}
